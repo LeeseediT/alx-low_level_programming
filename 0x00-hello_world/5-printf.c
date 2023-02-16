eca58331ae31bfa22d66c33c3f475b903752db33
Thato <thatolk16@gmail.com>
2023-02-16 10:46:22 -0800

    Using printf to print a line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - Using printf to print a line
+ *  Return: 0
+ */
+int main(void)
+{
+	prinf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
