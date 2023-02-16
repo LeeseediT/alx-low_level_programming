84f89e18f7c46a6d4cf98d977b533d272e7b1304
Thato <thatolk16@gmail.com>
2023-02-16 11:34:19 -0800

    Printing various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	long int l;
 	long long int li;
 	float f;
+
 printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu bytes(s)\n", sizeof(i));
 printf("Size of a long int: %lu bytes(s)\n", sizeof(l));
