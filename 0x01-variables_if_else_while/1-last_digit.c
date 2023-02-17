11c1f2bbfe9eded58bf9190c1fc0e9a3d1f1d49f
Thato <thatolk16@gmail.com>
2023-02-17 10:08:19 -0800

    Using a if loop and the last digit of a string

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,23 @@
 int main(void)
 {
 	int n;
-	int a;
+	int d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	a = n % 10;
+	d = n % 10;
 
-	if (a > 5)
+	if (d > 5)
 	{
-		printf("Last digit of %d and is greater than 5\n", n, a);
+		printf("Last digit of %d and %d is greater than 5\n", n, d);
 	}
-	else if (a == 0)
+	else if (d == 0)
 	{
-		printf("Last digit of %d and is 0\n", n, a);
+		printf("Last digit of %d and is %d 0\n", n, d);
 	}
 	else
 	{
-		printf("Last digit of %d and is less than 6 and not 0\n", n, a);
+		printf("Last digit of %d and is %d less than 6 and not 0\n", n, d);
 	}
 	return (0);
 }
