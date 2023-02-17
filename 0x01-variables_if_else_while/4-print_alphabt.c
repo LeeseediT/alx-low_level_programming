8eaa43cad8181b2746ccad946ee803c0e61d8cab
Thato <thatolk16@gmail.com>
2023-02-17 11:02:40 -0800

    for and if loop

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 /**
  * main - A program that prints the alphabet in lowercase
  * expect for q and e
  * Return: 0
  */
+
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'q' && !='e')
+		if (letter != 'e' && !='q')
 			putchar(letter);
 	}
 	putchar('\n');
