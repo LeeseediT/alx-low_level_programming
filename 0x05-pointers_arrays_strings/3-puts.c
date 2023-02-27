29a2c3db9ffd25c320ec52bd3e718af5f15d71a6
Thato <thatolk16@gmail.com>
2023-02-27 13:04:30 -0800

    Printing a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts -  function that prints a string to stdout
+ * @str: string to be printed
+ * Return: string
+ */
+void _puts(char *str)
+{
+	while (*str != 0)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
